c714115c07084af7da332111b6282d451a034f03
kabir-coder <72500963+kabir-coder@users.noreply.github.com>
2020-11-11 17:00:32 +0530

    Add files via upload

---
+++ gr.js
@@ -0,0 +1,22 @@
+class Ground{
+constructor(x,y,w,h) {
+var option={isStatic: true}  
+this.body=Bodies.rectangle(x,y,w,h,option)
+this.width=w;
+this.height=h;
+World.add(world,this.body)
+
+
+
+
+
+
+}
+display(){
+    var pos =this.body.position;
+    rectMode(CENTER);
+    fill(255);
+    rect(pos.x, pos.y, this.width, this.height);
+    
+}
+}
